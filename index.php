4f909fb03bf52448399592aa0d38a8e3e106bb85
reymar1992 <reymar@cazoomi.com>
2016-07-15 18:33:23 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Cazoomi Test 101</title>
+</head>
+<body>
+<form action="RetrieveContacts" method="get">
+<h2>Cazoomi Test 101 - Retrieve Contacts from Highrise API:</h2>
+<br>
+<p>Click here to retrieve Contacts:
+
+<input type="submit" value="Retrieve Contacts">
+
+
+</form>
+</body>
+</html>
